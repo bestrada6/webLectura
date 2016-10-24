41246d336652756b9acb506ee6c74ba4e55d0172
bestrada <bestrada@bestrada.ntcip.cipsa>
2016-10-24 14:33:32 -0500

    priemr commit

---
+++ webLectura/master/Footer.php
@@ -0,0 +1,25 @@
+<div class="btn-floating" id="help-actions">
+    <div class="btn-bg"></div>
+    <button type="button" class="btn btn-default btn-toggle" data-toggle="toggle" data-target="#help-actions">
+        <i class="icon fa fa-plus"></i>
+        <span class="help-text">Shortcut</span>
+    </button>
+    <div class="toggle-content">
+        <ul class="actions">
+            <li><a href="#">Website</a></li>
+            <li><a href="#">Documentation</a></li>
+            <li><a href="#">Issues</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </div>
+</div>
+
+<footer class="app-footer"> 
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="footer-copyright">
+                Copyright © 2016 Company Co,Ltd.
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ webLectura/master/SideBar.php
@@ -0,0 +1,64 @@
+
+
+<aside class="app-sidebar" id="sidebar">
+    <div class="sidebar-header">
+        <a class="sidebar-brand" href="#"><span class="highlight">Flat v3</span> Admin</a>
+        <button type="button" class="sidebar-toggle">
+            <i class="fa fa-times"></i>
+        </button>
+    </div>
+
+    <div class="sidebar-menu">
+        <ul class="sidebar-nav">
+            
+            <li class="active">
+                <a href="./index.html">
+                    <div class="icon">
+                        <i class="fa fa-tasks" aria-hidden="true"></i>
+                    </div>
+                    <div class="title">Dashboard</div>
+                </a>
+            </li>
+            
+            <li class="@@menu.messaging">
+                <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-comments" aria-hidden="true"></i>
+                    </div>
+                    <div class="title">Messaging</div>
+                </a>
+            </li>
+            
+            <li >
+                <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-comments" aria-hidden="true"></i>
+                    </div>
+                    <div class="title">Pedidos</div>
+                </a>
+            </li>
+            
+            <li >
+                <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-comments" aria-hidden="true"></i>
+                    </div>
+                    <div class="title">Reportes</div>
+                </a>
+            </li>
+            
+        </ul>
+    </div>
+
+    <div class="sidebar-footer">
+        <ul class="menu">
+            <li>
+                <a href="/" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-cogs" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li><a href="#"><span class="flag-icon flag-icon-th flag-icon-squared"></span></a></li>
+        </ul>
+    </div>
+</aside>
+
